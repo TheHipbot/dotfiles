204a68bdfc8310501d309fb38720afc2fb704272
Jeremy Chambers <jeremy@thehipbot.com>
2019-01-18 12:15:09 -0600

    Removes unnecessary pacman update, Fixes script to install package list

--- arch/scripts/install-packages.sh
+++ arch/scripts/install-packages.sh
@@ -3,7 +3,6 @@
 SCRIPT_PATH=$(dirname $0)
 
 # Update and install yay
-sudo pacman -Syy --noconfirm
 sudo pacman -Syy --noconfirm yay
 
-sudo yay -Sy --noconfirm --needed "$(cat $SCRIPT_PATH/package-list.txt)"
+sudo yay -Sy --noconfirm --needed - < "$SCRIPT_PATH/package-list.txt"
