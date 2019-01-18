145ed963c2bd0aa798c0bced3341003c27c471cb
Jeremy Chambers <jeremy@thehipbot.com>
2019-01-18 12:05:59 -0600

    Try something else for script path

--- arch/scripts/install-packages.sh
+++ arch/scripts/install-packages.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+$SCRIPT_PATH = $(dirname $0)
+
 # Update and install yay
 sudo pacman -Syy --noconfirm
 sudo pacman -Syy --noconfirm yay
 
-sudo yay -Sy --noconfirm --needed "$(cat $SCRIPTS_PATH/package-list.txt)"
+sudo yay -Sy --noconfirm --needed "$(cat $SCRIPT_PATH/package-list.txt)"
