d369c6ab5d37e7049bd57b00e2297590097667b6
Jeremy Chambers <jeremy@thehipbot.com>
2020-12-06 21:14:54 -0600

    Updated for ubuntu, Update README, Update rust installation

---
+++ ubuntu/scripts/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH=$(dirname $0)
+
+set -ex
+
+xargs -a $SCRIPT_PATH/packages.txt sudo apt-get install -y
+
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+curl https://pyenv.run | bash
